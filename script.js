cfa0e0ed461c32ac3bb766f76fe63de3b772cab9
slnwrld <142457252+slnwrld@users.noreply.github.com>
2023-08-28 15:36:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+  var dropdownMenu = document.getElementById("dropdownMenu");
+  dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
+}
